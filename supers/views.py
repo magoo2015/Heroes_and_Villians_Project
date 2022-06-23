baf281590c458f4b8856d1cbd803f3c6f515bcda
Sydney McGee <mcgee.sydney@gmail.com>
2022-06-22 23:13:30 -0500

    Created new view function for get supers by id + added new urls path for super detail view function

--- supers/views.py
+++ supers/views.py
@@ -16,4 +16,14 @@ def supers_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
         
+@api_view(['GET'])        
+def super_detail(request, pk):
+    try:
+        super = Super.objects.get(pk=pk)
+        serializer = SuperSerializer(super);
+        return Response(serializer.data)
         
+    except Super.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    
